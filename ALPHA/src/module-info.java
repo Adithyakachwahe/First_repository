94a9458808c323db3a3254409bc2791517b0ef6f
sourav p kachwahe <sourav p kachwahe@LAPTOP-ILSS8NG5>
2021-04-29 13:26:52 +0530

    FIRST_COMMIT

---
+++ ALPHA/src/module-info.java
@@ -0,0 +1,2 @@
+module alpha_module {
+}
---
+++ ALPHA/src/package_1/hello_world.java
@@ -0,0 +1,11 @@
+package package_1;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
